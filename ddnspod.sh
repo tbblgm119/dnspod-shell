8ce6c7903fa7045e2b103d182eebac8592a74586
tbblgm119 <569996437@qq.com>
2021-03-16 18:17:49 +0800

    Update ddnspod.sh

--- ddnspod.sh
+++ ddnspod.sh
@@ -5,13 +5,13 @@
 . /your_real_path/ardnspod
 
 # Combine your token ID and token together as follows
-arToken="12345,7676f344eaeaea9074c123451234512d"
+arToken="208252,4ffb384071d4fb7f7e377a2ed9568d46"
 
 # Place each domain you want to check as follows
 # you can have multiple arDdnsCheck blocks
 
 # IPv4:
-arDdnsCheck "test.org" "subdomain"
+arDdnsCheck "test.org" "omv"
 
 # IPv6:
-arDdnsCheck "test.org" "subdomain6" 6
+arDdnsCheck "test.org" "omv6" 6
